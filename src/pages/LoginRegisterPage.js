60f941bafeae271d872294c36dd5fa10953477ca
Ifan Alriansyah <fanalriansyah@gmail.com>
2021-07-23 11:24:15 +0700

    delete get method on login registerpage

--- src/pages/LoginRegisterPage.js
+++ src/pages/LoginRegisterPage.js
@@ -1,17 +1,8 @@
 import React from "react";
 import Login from "../components/register/Login";
 import Register from "../components/register/Register";
-import Axios from "axios";
 
 export default function LoginRegisterPage({ state }) {
-  Axios({
-    method: "GET",
-    url: "http://localhost:8000/register",
-    headers: {
-      "Content-Type": "appication/json",
-    },
-  }).then((res) => console.log(res.data.message));
-
   if (state) {
     return <Register />;
   }
